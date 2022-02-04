9bda469c1188ba5c6eb27eddbf5c2fd321e65125
smrutikapatil <smruti.patil17@gmail.com>
2022-02-04 21:05:24 +0530

    Added- Player rools die to get number between 1 to 6

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -3,6 +3,8 @@ package com.snakeandladder;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         int startPosition = 0;
+        int rollDice = (int) Math.floor(Math.random() * 6 + 1);
         System.out.println("Player is at initial position: " + startPosition);
+        System.out.println("After rolling dice we get : " + rollDice);
     }
 }
