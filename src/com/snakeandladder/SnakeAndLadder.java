524f2f79705d33942467b9888ae026c30cd7a54a
smrutikapatil <smruti.patil17@gmail.com>
2022-02-04 21:45:29 +0530

    Added- Play the game with 2 player

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -1,46 +1,66 @@
 package com.snakeandladder;
 
 public class SnakeAndLadder {
-    public static final int NO_PLAY = 1;
-    public static final int LADDER = 2;
-    public static final int SNAKE = 3;
+    public static final int NO_PLAY = 0;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
     public static final int WINNING_POSITION = 100;
+    public static final int START_POSITION = 0;
+    public static int chance = 0;
+    public static int playerOnePosition = 0;
+    public static int playerTwoPosition = 0;
+    public static int playerPosition = 0;
 
     public static void main(String[] args) {
+        switchPlayer();
+    }
 
-        int startPosition = 0;
-        int playerPosition = 0;
-        int diceCount = 0;
 
-        while(playerPosition < WINNING_POSITION) {
+    public static void switchPlayer(){
 
-            diceCount ++;
-            int rollDice = (int) Math.floor(Math.random() * 6 + 1);
-            int option = (int) Math.floor(Math.random() * 3 + 1);
-
-            switch (option) {
-                case NO_PLAY:
-                    System.out.println("Player is at same position:");
+        while (playerOnePosition < WINNING_POSITION && playerTwoPosition < WINNING_POSITION) {
+            if (chance % 2 == 0) {
+                playerPosition = playerOnePosition;
+                playGame();
+                playerOnePosition = playerPosition;
+                if (playerOnePosition == WINNING_POSITION) {
+                    System.out.println("Yey!Player One Won");
                     break;
-                case LADDER:
-                    playerPosition = playerPosition + rollDice;
-                    System.out.println("Player has got ladder so its increased by: "+ rollDice);
-                    if(playerPosition > 100){
-                        playerPosition = playerPosition -rollDice;
-                    }
+                }
+            } else {
+                playerPosition = playerTwoPosition;
+                playGame();
+                playerTwoPosition = playerPosition;
+                if (playerTwoPosition == WINNING_POSITION) {
+                    System.out.println("Yey! Player Two Won");
                     break;
-                case SNAKE:
+                }
+            }
+        }
+    }
+    public static void playGame(){
+        int rollDice = (int) Math.floor(Math.random() * 6 + 1);
+        int option = (int) Math.floor(Math.random() * 3);
+
+        switch (option) {
+            case NO_PLAY:
+                playerPosition = playerPosition;
+                System.out.println("Player  is at same position");
+                break;
+            case LADDER:
+                chance++;
+                playerPosition = playerPosition + rollDice;
+                System.out.println("Player position is increased by : " + rollDice);
+                if (playerPosition > 100) {
                     playerPosition = playerPosition - rollDice;
-                    System.out.println("Player has got ladder so its decreased by: "+ rollDice);
-                    if(playerPosition < 0){
-                        playerPosition = startPosition;
-                    }
-                    break;
-            }
-            if(playerPosition == 100){
-                System.out.println("Number of times dice was thrown: " + diceCount);
-                System.out.println("Final position of player: " + playerPosition);
-            }
+                }
+                break;
+            case SNAKE:
+                playerPosition = playerPosition - rollDice;
+                System.out.println("Player  position is decreased by : " + rollDice);
+                if (playerPosition < 0) {
+                    playerPosition = START_POSITION;
+                }
         }
     }
 }
