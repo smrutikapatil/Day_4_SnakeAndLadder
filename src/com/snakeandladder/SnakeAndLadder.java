8e453cdafd878ab487cb8aa6b7104e02923a4f30
smrutikapatil <smruti.patil17@gmail.com>
2022-02-04 20:59:52 +0530

    Added- Initilized start position

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -2,6 +2,7 @@ package com.snakeandladder;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        System.out.println("Welcome to snake and ladder game");
+        int startPosition = 0;
+        System.out.println("Player is at initial position: " + startPosition);
     }
 }
