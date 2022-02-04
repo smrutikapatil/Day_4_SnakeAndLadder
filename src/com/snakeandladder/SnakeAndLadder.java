52eb14decf0eeaa97977fceff562316d2c8786bb
smrutikapatil <smruti.patil17@gmail.com>
2022-02-04 21:40:15 +0530

    Added- Player to win the game

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -7,30 +7,40 @@ public class SnakeAndLadder {
     public static final int WINNING_POSITION = 100;
 
     public static void main(String[] args) {
+
         int startPosition = 0;
         int playerPosition = 0;
         int diceCount = 0;
+
         while(playerPosition < WINNING_POSITION) {
+
+            diceCount ++;
             int rollDice = (int) Math.floor(Math.random() * 6 + 1);
             int option = (int) Math.floor(Math.random() * 3 + 1);
 
             switch (option) {
                 case NO_PLAY:
+                    System.out.println("Player is at same position:");
                     break;
                 case LADDER:
                     playerPosition = playerPosition + rollDice;
+                    System.out.println("Player has got ladder so its increased by: "+ rollDice);
                     if(playerPosition > 100){
-                        playerPosition = playerPosition - rollDice;
+                        playerPosition = playerPosition -rollDice;
                     }
                     break;
                 case SNAKE:
                     playerPosition = playerPosition - rollDice;
+                    System.out.println("Player has got ladder so its decreased by: "+ rollDice);
                     if(playerPosition < 0){
                         playerPosition = startPosition;
                     }
                     break;
             }
+            if(playerPosition == 100){
+                System.out.println("Number of times dice was thrown: " + diceCount);
+                System.out.println("Final position of player: " + playerPosition);
+            }
         }
-        System.out.println("Player has reached has reached exact Winning position!:" + playerPosition);
     }
 }
