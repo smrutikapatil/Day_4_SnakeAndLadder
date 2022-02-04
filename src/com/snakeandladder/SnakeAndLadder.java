e593f2699d6b0afde4696896891a6f5cf75debfd
smrutikapatil <smruti.patil17@gmail.com>
2022-02-04 21:35:39 +0530

    Added- Now player has reaches exact winning position

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -1,39 +1,36 @@
 package com.snakeandladder;
 
 public class SnakeAndLadder {
-            public static final int NO_PLAY = 1;
-            public static final int LADDER = 2;
-            public static final int SNAKE = 3;
-            public static final int WINNING_POSITION = 100;
+    public static final int NO_PLAY = 1;
+    public static final int LADDER = 2;
+    public static final int SNAKE = 3;
+    public static final int WINNING_POSITION = 100;
 
-            public static void main(String[] args) {
+    public static void main(String[] args) {
+        int startPosition = 0;
+        int playerPosition = 0;
+        int diceCount = 0;
+        while(playerPosition < WINNING_POSITION) {
+            int rollDice = (int) Math.floor(Math.random() * 6 + 1);
+            int option = (int) Math.floor(Math.random() * 3 + 1);
 
-                int startPosition = 0;
-                int playerPosition = 0;
-                int diceCount = 0;
-
-                while(playerPosition < WINNING_POSITION) {
-
-                    int rollDice = (int) Math.floor(Math.random() * 6 + 1);
-                    int option = (int) Math.floor(Math.random() * 3 + 1);
-
-                    switch (option) {
-                        case NO_PLAY:
-                            break;
-                        case LADDER:
-                            playerPosition = playerPosition + rollDice;
-                            if(playerPosition > 100){
-                                playerPosition = playerPosition -rollDice;
-                            }
-                            break;
-                        case SNAKE:
-                            playerPosition = playerPosition - rollDice;
-                            if(playerPosition < 0){
-                                playerPosition = startPosition;
-                            }
-                            break;
+            switch (option) {
+                case NO_PLAY:
+                    break;
+                case LADDER:
+                    playerPosition = playerPosition + rollDice;
+                    if(playerPosition > 100){
+                        playerPosition = playerPosition - rollDice;
                     }
-                }
-                System.out.println("  Winning Position:" + playerPosition);
+                    break;
+                case SNAKE:
+                    playerPosition = playerPosition - rollDice;
+                    if(playerPosition < 0){
+                        playerPosition = startPosition;
+                    }
+                    break;
             }
+        }
+        System.out.println("Player has reached has reached exact Winning position!:" + playerPosition);
     }
+}
