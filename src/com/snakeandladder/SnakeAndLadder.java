9272864149a5fa9e36d1693dd84efc47d1ec1972
smrutikapatil <smruti.patil17@gmail.com>
2022-02-04 21:15:44 +0530

    Added- Created three option playing game

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -1,10 +1,26 @@
 package com.snakeandladder;
 
 public class SnakeAndLadder {
-    public static void main(String[] args) {
-        int startPosition = 0;
-        int rollDice = (int) Math.floor(Math.random() * 6 + 1);
-        System.out.println("Player is at initial position: " + startPosition);
-        System.out.println("After rolling dice we get : " + rollDice);
+        public static final int NO_PLAY = 1;
+        public static final int LADDER = 2;
+        public static final int SNAKE = 3;
+
+        public static void main (String[]args){
+            int startPosition = 0;
+            int playerPosition = 0;
+            int rollDice = (int) Math.floor(Math.random() * 6 + 1);
+            int option = (int) Math.floor(Math.random() * 3 + 1);
+
+            switch (option) {
+                case NO_PLAY:
+                    System.out.println("Player is at same position");
+                    break;
+                case LADDER:
+                    System.out.println("Player's position is increased by: " + rollDice);
+                    break;
+                case SNAKE:
+                    System.out.println("Player's position is decreased by: " + rollDice);
+                    break;
+            }
+        }
     }
-}
