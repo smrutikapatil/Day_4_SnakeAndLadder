961143576855b8a032299f1985be4da3908b7c74
smrutikapatil <smruti.patil17@gmail.com>
2022-02-04 21:27:46 +0530

    Added- Added - Repeating loop till player reaches winning position

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -1,26 +1,39 @@
 package com.snakeandladder;
 
 public class SnakeAndLadder {
-        public static final int NO_PLAY = 1;
-        public static final int LADDER = 2;
-        public static final int SNAKE = 3;
+            public static final int NO_PLAY = 1;
+            public static final int LADDER = 2;
+            public static final int SNAKE = 3;
+            public static final int WINNING_POSITION = 100;
 
-        public static void main (String[]args){
-            int startPosition = 0;
-            int playerPosition = 0;
-            int rollDice = (int) Math.floor(Math.random() * 6 + 1);
-            int option = (int) Math.floor(Math.random() * 3 + 1);
+            public static void main(String[] args) {
 
-            switch (option) {
-                case NO_PLAY:
-                    System.out.println("Player is at same position");
-                    break;
-                case LADDER:
-                    System.out.println("Player's position is increased by: " + rollDice);
-                    break;
-                case SNAKE:
-                    System.out.println("Player's position is decreased by: " + rollDice);
-                    break;
+                int startPosition = 0;
+                int playerPosition = 0;
+                int diceCount = 0;
+
+                while(playerPosition < WINNING_POSITION) {
+
+                    int rollDice = (int) Math.floor(Math.random() * 6 + 1);
+                    int option = (int) Math.floor(Math.random() * 3 + 1);
+
+                    switch (option) {
+                        case NO_PLAY:
+                            break;
+                        case LADDER:
+                            playerPosition = playerPosition + rollDice;
+                            if(playerPosition > 100){
+                                playerPosition = playerPosition -rollDice;
+                            }
+                            break;
+                        case SNAKE:
+                            playerPosition = playerPosition - rollDice;
+                            if(playerPosition < 0){
+                                playerPosition = startPosition;
+                            }
+                            break;
+                    }
+                }
+                System.out.println("  Winning Position:" + playerPosition);
             }
-        }
     }
